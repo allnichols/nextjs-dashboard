696142547d5ab2c8673500e4c6db0726025882b1
Max <allenmaxnichols@gmail.com>
2025-05-05 10:39:18 -0500

    add links

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import { UserGroupIcon } from "@heroicons/react/24/outline";
+import {
+  DocumentDuplicateIcon,
+  HomeIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-
+const links = [
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
+  {
+    name: "Invoices",
+    href: "/dashboard/invoices",
+    icon: DocumentDuplicateIcon,
+  },
+  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+];
 export default function NavLinks() {
   const pathname = usePathname();
   return (
